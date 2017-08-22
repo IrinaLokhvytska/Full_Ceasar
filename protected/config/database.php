449b98aaec99058d57842bab6f06debb0fc132db
voldan <vladdan1980@gmail.com>
2017-08-22 11:19:08 +0300

    groupsList connecting to db

--- protected/config/database.php
+++ protected/config/database.php
@@ -1,14 +1,11 @@
 <?php
-
 // This is the database connection configuration.
 return [
     'connectionString' => 'sqlite:' . dirname(__FILE__) . '/../data/testdrive.db',
     // uncomment the following lines to use a MySQL database
-
     'connectionString' => 'mysql:host=localhost;dbname=caesar',
     'emulatePrepare' => true,
     'username' => 'root',
     'password' => 'admin',
     'charset' => 'utf8',
-
 ];
--- protected/models/GroupsList.php
+++ protected/models/GroupsList.php
@@ -12,60 +12,72 @@ class GroupsList extends CActiveRecord
         return 'groups';
     }
 
+    public function requestParametersParse($params)
+    {
+        if (strpos($params, ',')) {
+            $params = explode(',', $params);
+        } else {
+            $params = [$params];
+        }
+
+        return $params;
+    }
+
     public function getGroupsList($locations)
     {
-//        if (!isset($locations) || $locations === 'undefined') {
-//            $locations = [UserSessionInfo::getUserLocationId()];
-//        }
+        $locations = $this->requestParametersParse($locations);
 
-//        $locations = [1];
+        if (!isset($locations) || $locations === 'undefined') {
+            $locations = [2];
+        }
+
+        $criteria = new CDbCriteria();
+        $criteria->select = 'name, direction';
+        $criteria->addInCondition('location', $locations);
+        $groupsList = GroupsList::model()->findAll($criteria);
 //
-//        $criteria = new CDbCriteria();
-//        $criteria->select = 'name, direction';
-//        $criteria->addInCondition('location_id', $locations);
-//        $groupsList = GroupsList::model()->findAll($criteria);
-        if (strpos($locations, ',')) {
-            $locations = explode(',', $locations);
-        }
-
-
-        $groupsList = [
-            ['groupName' => 'LV17', 'groupDirection' => 'Java'],
-            ['groupName' => 'DP19', 'groupDirection' => 'PHP'],
-            ['groupName' => 'KH20', 'groupDirection' => 'JS'],
-            ['groupName' => 'ZP11', 'groupDirection' => 'JS'],
-            ['groupName' => 'DP12', 'groupDirection' => 'Java'],
-            ['groupName' => 'RV18', 'groupDirection' => 'PHP'],
-            ['groupName' => 'CH14', 'groupDirection' => 'GO'],
-            ['groupName' => 'KV13', 'groupDirection' => 'Java'],
-            ['groupName' => 'LV22', 'groupDirection' => 'JS'],
-            ['groupName' => 'KH24', 'groupDirection' => 'PHP'],
-            ['groupName' => 'ZP21', 'groupDirection' => 'Java'],
-            ['groupName' => 'DP23', 'groupDirection' => 'JS'],
-        ];
-
-        if (is_array($locations)) {
-            if ($locations[0] === 'DP') {
-                $groupsList = [
-                    ['groupName' => 'LV17', 'groupDirection' => 'Java'],
-                    ['groupName' => 'DP19', 'groupDirection' => 'PHP'],
-                    ['groupName' => 'KH20', 'groupDirection' => 'JS'],
-                ];
-            }
-            if ($locations[1] === 'LV')
-            {
-                $groupsList[] = ['groupName' => 'LV22', 'groupDirection' => 'JS'];
-            }
-        } else {
-            if ($locations === 'DP') {
-                $groupsList = [
-                    ['groupName' => 'LV17', 'groupDirection' => 'Java'],
-                    ['groupName' => 'KH20', 'groupDirection' => 'JS'],
-                ];
-            }
-        }
-
-
+//        if (strpos($locations, ',')) {
+//            $locations = explode(',', $locations);
+//        }
+//
+//
+//        $groupsList = [
+//            ['groupName' => 'LV17', 'groupDirection' => 'Java'],
+//            ['groupName' => 'DP19', 'groupDirection' => 'PHP'],
+//            ['groupName' => 'KH20', 'groupDirection' => 'JS'],
+//            ['groupName' => 'ZP11', 'groupDirection' => 'JS'],
+//            ['groupName' => 'DP12', 'groupDirection' => 'Java'],
+//            ['groupName' => 'RV18', 'groupDirection' => 'PHP'],
+//            ['groupName' => 'CH14', 'groupDirection' => 'GO'],
+//            ['groupName' => 'KV13', 'groupDirection' => 'Java'],
+//            ['groupName' => 'LV22', 'groupDirection' => 'JS'],
+//            ['groupName' => 'KH24', 'groupDirection' => 'PHP'],
+//            ['groupName' => 'ZP21', 'groupDirection' => 'Java'],
+//            ['groupName' => 'DP23', 'groupDirection' => 'JS'],
+//        ];
+//
+//        if (is_array($locations)) {
+//            if ($locations[0] === 'DP') {
+//                $groupsList = [
+//                    ['groupName' => 'LV17', 'groupDirection' => 'Java'],
+//                    ['groupName' => 'DP19', 'groupDirection' => 'PHP'],
+//                    ['groupName' => 'KH20', 'groupDirection' => 'JS'],
+//                ];
+//            }
+//            if ($locations[1] === 'LV')
+//            {
+//                $groupsList[] = ['groupName' => 'LV22', 'groupDirection' => 'JS'];
+//            }
+//        } else {
+//            if ($locations === 'DP') {
+//                $groupsList = [
+//                    ['groupName' => 'LV17', 'groupDirection' => 'Java'],
+//                    ['groupName' => 'KH20', 'groupDirection' => 'JS'],
+//                ];
+//            }
+//        }
+//
+//
 
         return $groupsList;
     }
