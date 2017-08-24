3c5827ab3f2e5f263d8d69d86dce56564de39197
voldan <vladdan1980@gmail.com>
2017-08-24 11:32:41 +0300

    GroupComponent complete + demo/groups filled with new groups

--- protected/components/GroupComponent.php
+++ protected/components/GroupComponent.php
@@ -2,15 +2,19 @@
 
 class GroupComponent extends CApplicationComponent
 {
-    public function getList($locations)
+    public function getList($locationNames)
     {
+        if ($locationNames !== 'undefined') {
+            $locationNames = json_encode($locationNames);
+            $locations = $this->getLocationsId($locationNames);
+        } else {
+            $locations = [Yii::app()->user->location];
+        }
+
         $criteria = new CDbCriteria();
         $criteria->alias = 'group';
         $criteria->select = "{$criteria->alias}.name";
-        if ($locations !== 'undefined') {
-            $locations = json_decode($locations);
-            $criteria->addInCondition('location', $locations);
-        }
+        $criteria->addInCondition('location', $locations);
         /** @var Group[] $rows */
         $rows = Group::model()->with('direction')->findAll($criteria);
 
@@ -26,5 +30,25 @@ class GroupComponent extends CApplicationComponent
         }
 
         return empty($result) ? [] : $result;
+    }
+
+    public function getLocationsId($locationNames)
+    {
+        $locationIdList = [];
+        $locationNames = explode(',', $locationNames);
+
+        $criteria = new CDbCriteria();
+        $criteria->select = 'id, full_name';
+        $locationList = Locations::model()->findAll($criteria);
+
+        foreach ($locationNames as $locationName) {
+            foreach ($locationList as $locationId) {
+                if ($locationName === $locationId['full_name']) {
+                    $locationIdList[] = $locationId['id'];
+                }
+            }
+        }
+
+        return $locationIdList;
     }
 }
