2868217656a59f928acdd63aab29aee463ca9604
Cruelkid <akosovich@narod.ru>
2017-08-24 22:49:35 +0300

    DONT TOUCH MIGRATIONSgit add .

--- protected/migrations/m170816_120012_create_users_table.php
+++ protected/migrations/m170816_120012_create_users_table.php
@@ -12,8 +12,8 @@ class m170816_120012_create_users_table extends CDbMigration
             `username` VARCHAR(45) NOT NULL,
             `password` VARCHAR(45) NOT NULL,
             `location_id` INT NOT NULL,
+            `picture` VARCHAR(100) NOT NULL,
             `type` VARCHAR(45) NOT NULL,
-            `picture` VARCHAR(200) NOT NULL,
             PRIMARY KEY (`id`),
             UNIQUE `username` (`username`)) ENGINE = InnoDB DEFAULT CHARSET=utf8;
         ');
